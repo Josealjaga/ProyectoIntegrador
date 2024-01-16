a0fe6e36c6a473f3242352429b9f87416e901451
Josealjaga <josealjaga97@hotmail.com>
2024-01-16 18:39:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+class Personas {
+  nombre;
+  apellido;
+  edad;
+  cedula;
+  constructor(nombre,apellido, edad, cedula) {
+    this.nombre = Jose;
+    this.apellido = Jaraba;
+    this.edad = 26;
+    this.cedula = 123456789;
+} 
+}
+class Animales {
+  nombre;
+  genero;
+  raza;
+  constructor(nombre, genero, raza) {
+    this.nombre = "Perro";
+    this.genero = "Macho";
+    this.raza = "Pastor Aleman";
+  }
+}
+class Veterinaria {
+  nombre;
+  direccion;
+  telefono;
+  constructor(nombre, direccion, telefono) {
+    this.nombre = "Veterinaria";
+    this.direccion = "Calle 123";
+    this.telefono = 123456789;
+  }
+}
